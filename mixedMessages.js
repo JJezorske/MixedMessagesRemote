93c15622d602959f97b19b254de7b1bdf145e0a9
JJ <79755422+JJezorske@users.noreply.github.com>
2021-03-10 16:12:04 +0000

    creation of random5 function

--- mixedMessages.js
+++ mixedMessages.js
@@ -6,3 +6,16 @@ const verbs = ['like', 'hate', 'prefer', 'encourage', 'ignore']
 
 const nouns= ['apples!', 'vinegar!', 'groot?', 'people', 'capitalism' ]
 
+const random5 = arr => {
+        let num = Math.floor(Math.random ()* 5)
+        return arr[num]
+};
+
+console.log(random5(verbs))
+
+const randomPronoun = () => {
+    for (let i = 0; i < pronouns.length; i++) {
+        console.log(pronouns[i])
+    }
+};
+//

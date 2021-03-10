868668318039226a0b877923314c1e955aea72c2
JJ <79755422+JJezorske@users.noreply.github.com>
2021-03-10 17:12:32 +0000

    object method completed

--- mixedMessages.js
+++ mixedMessages.js
@@ -8,18 +8,12 @@ const mixedMessages = {
         let num = Math.floor(Math.random ()* (arr.length))
         return arr[num]
     },
-}
-
-       
-
-//console.log(random5(verbs))
-
-const randomMessage = (arr1, arr2, arr3) => {
-    let name = random5(arr1);
-    let verb = random5(arr2);
-    let noun = random5(arr3);
-    return `${name} ${verb} ${noun}`
+    generateMessage() {
+        const name = this.getRandomWord(this._names)
+        const verb = this.getRandomWord(this._verbs)
+        const noun = this.getRandomWord(this._nouns)
+        return `${name} ${verb} ${noun}`
+    },
 };
-
-console.log(randomMessage(names, verbs, nouns))
-//
+   
+console.log(mixedMessages.generateMessage())
